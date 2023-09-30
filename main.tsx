9018d0833d27f3eac67b48d8c91b9acead3629e9
fxadilima <xaverius2017@gmail.com>
2023-09-30 18:06:34 +0700

    make a documentations from the start

--- main.tsx
+++ main.tsx
@@ -1,16 +1,9 @@
 /** @jsxImportSource https://esm.sh/preact */
-import express from "npm:express@4.18.2";
-import render from "https://esm.sh/preact-render-to-string@6.2.1";
-import runtime from "https://esm.sh/preact/jsx-runtime";
+import express from "https://esm.sh/v132/express@4.18.2";
+import renderToString from "https://esm.sh/v132/preact-render-to-string@6.2.1/src/index.js";
 
-function Card(props) {
-    return (
-    <div class="w3-container w3-card w3-round">
-    <h1>{props.title}</h1>
-    <p>{props.message}</p>
-    </div>
-    );
-}
+const PORT = 4000;
+const app = express();
 
 function Home(props) {
     return (
@@ -23,46 +16,23 @@ function Home(props) {
         <link rel="icon" type="image/svg+xml" href="/book.svg"/>
         </head>
         <body>
-            <div class="w3-content w3-padding-64" style="width:50%;">
-                <Card title={props.title} message={props.message}/>
-                <div class="w3-display-container w3-hover-opacity w3-padding-16">
-                    <div class="w3-container w3-border w3-animate-top w3-round-large" id="hello"></div>
-                </div>
-                <div class="w3-bar-block">
-                    <p><a class="w3-bar-item w3-btn w3-hover-red" href="/sub/test1">Go to test1</a></p>
-                    <p><button class="w3-btn w3-bar-item" type="button" id="btnTest">Test Event</button></p>
-                    <p><button class="w3-btn w3-bar-item" type="button" id="btnErase">Clear</button></p>
-                </div>
-                <script src="/modules/test1.js"></script>
-            </div>
+            <main class="w3-main w3-padding-32">
+                <header class="w3-container w3-content w3-card-4" style="width:50%;">
+                    <h1>{props.title}</h1>
+                    <p>{props.message}</p>
+                </header>
+            </main>
         </body>
         </html>
     );
 }
 
-const app = express();
+app.use(express.static(Deno.cwd() + "/public"));
 
 app.get("/", (req: Request, res: Response) => {
-    console.log(`Referrer: ${req.referrer}`);
-    const html = `<!DOCTYPE html>${render(<Home title="Hello" message="This is a test from Deno."/>)}`;
-    res.send(html);
-})
-.get("/sub/test1", async (req: Request, res: Response) => {
-    const d = await Deno.readTextFile(Deno.cwd() + "/public/modules/test1.js");
     res.type("text/html");
-    res.send(`<div><strong>Isi test1.js:</strong><pre><code>${d}</code></pre></div><a href="/">Back Home</a>`);
-})
-.get("/modules/*", async (req: Request, res: Response) => {
-    const d = await Deno.readTextFile(Deno.cwd() + "/public/modules/test1.js");
-    res.type("text/javascript");
-    res.send(d);
-})
-.get("/book.svg", async (req: Request, res: Response) => {
-    const d = await Deno.readFile(Deno.cwd() + "/public/book.svg");
-    console.log(`req.url: ${req.url}`);
-    res.type("image/svg+xml");
-    res.send(d);
+    res.send("<!doctype html>\n" + renderToString(<Home title="😁 Hello" message="Apa kabar?"/>));
 });
 
-console.log("Listening to PORT 10000");
-app.listen(10000);
+console.log(`Listening to port: ${PORT}`);
+app.listen(PORT);

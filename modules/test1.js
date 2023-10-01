12bb76cc35d8b0208e41cf721c7a33c3a1190cbc
fxadilima <xaverius2017@gmail.com>
2023-10-01 16:32:41 +0700

    change copied public/modules to root

---
+++ modules/test1.js
@@ -0,0 +1,13 @@
+
+//console.log("test1.js loaded...");
+document.getElementById("hello").innerHTML = "<h2>👁 Watch:</h2><pre>test1.js loaded...</pre>";
+
+document.getElementById("btnTest").addEventListener("click", () => {
+    document.getElementById("hello").innerHTML = "<p>Hi, there! 😎<br/>This is a test...</p>";
+    document.getElementById("hello").style.display = "block";
+});
+
+document.getElementById("btnErase").addEventListener("click", () => {
+    document.getElementById("hello").style.display = "none";
+});
+
